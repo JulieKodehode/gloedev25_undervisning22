e2c699b95b4702b65f4fc824e26f6874c08aac4d
JulieKodehode <julie.kodehode@gmail.com>
2025-06-18 11:00:16 +0200

    Mid lesson

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
 console.log("Hallajen");
+
+// Funksjon som henter og tar i mot data fra en Cat Facts API
+
+async function fetchFacts() {
+    const fetchData = await fetch("https://catfact.ninja/facts");
+    // console.log(fetchData);
+
+    const data = await fetchData.json();
+    // console.log(data);
+    // console.log(data.data); // Jobber fra dette punktet
+    // console.log(data.data[1]);
+    // console.log(data.data[1].fact);
+
+    return data;
+}
+
+// fetchFacts();
+
+async function displayFacts() {
+    const response = await fetchFacts();
+    console.log(response);
+
+    const catFacts = response.data;
+    console.log(catFacts);
+
+    const factsList = document.getElementById("factsList");
+
+    catFacts.forEach(item => {
+        const listItem = document.createElement("li");
+
+        const factItem = item.fact;
+        listItem.textContent = factItem;
+
+        factsList.appendChild(listItem);
+    })
+}
+
+displayFacts();
